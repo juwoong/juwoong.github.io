9f270f9d093b860cf3e522eb6344a61128f1fd96
Juwoong Bae <to@juwoong.me>
2023-02-18 00:20:34 +0900

    Updates

---
+++ component---src-pages-404-tsx-96387ee4a40f7c99ce98.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkminimal_blog=self.webpackChunkminimal_blog||[]).push([[218],{2513:function(e,l,n){n.r(l);var t=n(7294),u=n(74);l.default=e=>t.createElement(u.Z,null,t.createElement("h1",null,"404 - Page Not Found"),t.createElement("p",null,"Unfortunately we couldn't find what you were looking for :("))}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-96387ee4a40f7c99ce98.js.map
