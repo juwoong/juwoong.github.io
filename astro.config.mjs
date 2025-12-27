1b144645b209f697edb63ebdefd122ec9391dad9
Juwoong Bae <juungbae@gmail.com>
2025-12-28 04:38:41 +0900

    deploy netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,17 @@ import { defineConfig } from 'astro/config';
 import rehypeKatex from 'rehype-katex';
 import remarkMath from 'remark-math';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blog.juwoong.me',
-	integrations: [mdx(), sitemap()],
-	markdown: {
-		remarkPlugins: [remarkMath],
-		rehypePlugins: [rehypeKatex],
+  site: 'https://blog.juwoong.me',
+  integrations: [mdx(), sitemap()],
+
+  markdown: {
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
 	},
+
+  adapter: netlify(),
 });
